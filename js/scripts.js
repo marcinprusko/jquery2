ecdb6eca4333c4297713956803c5839f7582e84a
Michał Gacek <gacek.michael@outlook.com>
2017-03-15 07:52:55 +0100

    Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,17 @@
 $(function(){
 	//ten kod wykona się po załadowaniu DOM
 	var carouselList = $("#carousel ul");
-	setInterval(moveFirstSlide, 3000); //co 3 sek. wykona funkcję zmieniającą slajd. 
-	carouselList.animate({'marginLeft':-400}, 300, moveFirstSlide);
+	
+    setInterval(changeSlide, 3000); //co 3 sek. wykona funkcję zmieniającą slajd. 
+
+    function changeSlide() {
+    carouselList.animate({'marginLeft':-400}, 300, moveFirstSlide);
+}
+
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    }
 });
-
-$(function moveFirstSlide(){
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
-});
-
-
-
